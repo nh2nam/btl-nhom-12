b1df7b43c4bd34c99c197f47524c6898e4165523
Minh DevOps <minhco141207@gmail.com>
2026-05-15 13:03:47 +0700

    sửa lại phần giá khởi diem

--- dev/auction_server/src/main/java/com/auction/network/ClientHandler.java
+++ dev/auction_server/src/main/java/com/auction/network/ClientHandler.java
@@ -236,7 +236,8 @@ public class ClientHandler implements Runnable {
                     map.put("itemImagePath", item.getImagePath());
 
                     map.put("category", item.getCategory());
-                    map.put("startingPrice", auction.getCurrentHighestBid());
+                    map.put("itemStartingPrice", item.getStartingPrice()); // giá khởi điểm gốc của item
+                    map.put("startingPrice", auction.getCurrentHighestBid()); // giá hiện tại đang đấu giá
                     map.put("status", auction.getStatus());
 
                     String formattedEnd = auction.getEndTime().toString().replace("T", " ");
